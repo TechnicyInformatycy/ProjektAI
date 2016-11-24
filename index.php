6de035b969ba66d2807c02193a68b065cf4745a3
kamil559 <kamil.daniewski@gmail.com>
2016-11-24 21:00:36 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -26,13 +26,18 @@ if(isset($_GET["action"])){
 	}
 }
 $DIID=0;
+
+ 
+
 ?>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+
+</script>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -343,6 +348,7 @@ a:hover, a:active, a:focus {
 	window.setInterval(DIvUpdater, 3000);
 	function DIvUpdater(){
 		$(".chat-message").load("chatgenerator.php");
+		
 	}
 	function SenWiad(){
 		var wiadomosc = $("#imputwiad").val();
@@ -355,12 +361,21 @@ a:hover, a:active, a:focus {
 					if(data="1"){
 						$("#imputwiad").val("");
 						$(".chat-message").load("chatgenerator.php");
+						
 					}else{
 						alert(data);
 					}
 				}
 			);
 	}
+	
+
+
+$('#div1').scrollTop($('#div1')[0].scrollHeight);
+
+	
+
+	
 	</script>
   </body>
 </html>
